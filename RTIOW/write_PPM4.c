069f4d20185c09786f3f14ed4b4bb22464cd25a6
Ross Wilson <rzzzwilson@gmail.com>
2016-08-07 09:44:50 +0700

    Remove debug

--- RTIOW/write_PPM4.c
+++ RTIOW/write_PPM4.c
@@ -16,18 +16,6 @@
  * Decide if ray 'r' intersects a sphere.
  */
 
-//def hit_sphere(center, radius, r):
-//    """Decide if ray 'r' intersects a sphere."""
-//
-//    oc = r.origin - center
-//    a = r.direction.dot(r.direction)
-//    b = oc.dot(r.direction) * 2.0
-//    c = oc.dot(oc) - radius * radius
-//    discriminant = b * b - 4 * a * c
-//
-//    return discriminant > 0
-
-
 bool
 hit_sphere(vec3 *center, float radius, ray *r)
 {
@@ -46,10 +34,6 @@ color(ray *r)
 {
     if (hit_sphere(vec3_new_vals(0.0, 0.0, -1.0), 0.5, r))
         return vec3_new_vals(1.0, 0.0, 0.0);
-
-//    if hit_sphere(Vec3((0, 0, -1)), 0.5, r):
-//        return Vec3((1, 0, 0))
-
 
     vec3 *unit_direction = vec3_unit_vector(ray_direction(r));
     float t = 0.5 * (vec3_y(unit_direction) + 1.0);
