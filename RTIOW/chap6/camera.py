340a27b6cb6646405afe134c4d02037fe4839f84
Ross Wilson <rzzzwilson@gmail.com>
2016-08-14 15:12:48 +0700

    Antialiasing working

--- RTIOW/chap6/camera.py
+++ RTIOW/chap6/camera.py
@@ -3,6 +3,7 @@
 
 """
 A Camera class.
+From Chapter 6 of "Ray Tracing in one Weekend".
 """
 
 from vec3 import Vec3
@@ -17,10 +18,10 @@ class Camera(object):
     def __init__(self):
         """A camera clas."""
 
-        self.lower_left_corner = Vec3((-2.0, -1.0, -1.0))
-        self.horizontal = Vec3((4.0, 0.0, 0.0))
-        self.vertical = Vec3((0.0, 2.0, 0.0))
-        self.origin = Vec3((0.0, 0.0, 0.0))
+        self.origin = Vec3(0.0, 0.0, 0.0)
+        self.lower_left_corner = Vec3(-2.0, -1.0, -1.0)
+        self.horizontal = Vec3(4.0, 0.0, 0.0)
+        self.vertical = Vec3(0.0, 2.0, 0.0)
 
     def get_ray(self, u, v):
         return Ray(self.origin, self.lower_left_corner + self.horizontal*u
--- RTIOW/chap6/main.py
+++ RTIOW/chap6/main.py
@@ -3,9 +3,8 @@
 
 """
 A simple program to write a PPM file.
-
-From chapter 5 of "Ray Tracing in One Weekend".
-Surface normals ans multiple objects.
+From chapter 6 of "Ray Tracing in One Weekend".
+Antialiasing.
 """
 
 import sys
@@ -13,7 +12,6 @@ from math import sqrt
 from random import random
 
 from vec3 import Vec3
-import ray
 from hitable import Hitable, HitRecord
 from hitable_list import HitableList
 from sphere import Sphere
@@ -28,11 +26,11 @@ def color(r, world):
 
     rec = HitRecord()
     if world.hit(r, 0.0, sys.float_info.max, rec):
-        return Vec3((rec.normal.x + 1.0, rec.normal.y + 1.0, rec.normal.z + 1.0)) * 0.5
+        return Vec3(rec.normal.x + 1.0, rec.normal.y + 1.0, rec.normal.z + 1.0) * 0.5
     else:
         unit_direction = r.direction.unit_vector()
         t = 0.5 * (unit_direction.y + 1.0)
-        return Vec3((1.0, 1.0, 1.0)) * (1.0 - t) + Vec3((0.5, 0.7, 1.0)) * t
+        return Vec3(1.0, 1.0, 1.0) * (1.0 - t) + Vec3(0.5, 0.7, 1.0) * t
 
 nx = 200
 ny = 100
@@ -40,27 +38,28 @@ ns = 100
 
 print('P3\n%d %d 255' % (nx, ny))
 
-lower_left_corner = Vec3((-2.0, -1.0, -1.0))
-horizontal = Vec3((4.0, 0.0, 0.0))
-vertical = Vec3((0.0, 2.0, 0.0))
-origin = Vec3((0.0, 0.0, 0.0))
+#lower_left_corner = Vec3(-2.0, -1.0, -1.0)
+#horizontal = Vec3(4.0, 0.0, 0.0)
+#vertical = Vec3(0.0, 2.0, 0.0)
+#origin = Vec3(0.0, 0.0, 0.0)
 
-hlist = [Sphere(Vec3((0.0, 0.0, -1.0)), 0.5), Sphere(Vec3((0.0, -100.5, -1.0)), 100.0)]
+hlist = [Sphere(Vec3(0.0, 0.0, -1.0), 0.5), Sphere(Vec3(0.0, -100.5, -1.0), 100.0)]
 world = HitableList(hlist)
 
 cam = Camera()
 
 for j in range(ny)[::-1]:
     for i in range(nx):
-        col = Vec3((0.0, 0.0, 0.0))
+        col = Vec3(0.0, 0.0, 0.0)
         for s in range(ns):
-            u = (float(i) * random()) / float(nx)
-            v = (float(j) * random()) / float(ny)
+            u = (float(i) + random()) / float(nx)
+            v = (float(j) + random()) / float(ny)
             r = cam.get_ray(u, v)
             p = r.point_at_parameter(2.0)
             col += color(r, world)
 
         col /= float(ns)
+
         ir = int(255.99 * col.r)
         ig = int(255.99 * col.g)
         ib = int(255.99 * col.b)
