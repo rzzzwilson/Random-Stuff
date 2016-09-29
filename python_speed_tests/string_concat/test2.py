70e81872ca18028498a66d96b5eba327534ea9b3
Ross Wilson <rzzzwilson@gmail.com>
2016-09-30 01:03:18 +1000

    Changed the optimization defeat idea

--- python_speed_tests/string_concat/test2.py
+++ python_speed_tests/string_concat/test2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-"""Run simple string concatenation speed tests.
+"""Run string concatenation speed tests.
 
-The tests here are somewhat similar to those in test.py except we
-try to defeat any optimisations the python runtime does by doing
-the concatenation in a function.
+Sort of the same as test.py, but try to 'hide' the string concatenation
+from python optimization.  The basic idea is to take the code normally executed
+inside the loop and move it outside where it is compiled.  Then inside the loop
+just 'exec' the compiled code.  This appears to be enough to defeat the
+optimixation.
 """
 
 
@@ -14,75 +16,94 @@ from array import array
 try:
     from cStringIO import StringIO
 except ImportError:
+    # python3 - make changes
     import io
     global StringIO
     StringIO = io.StringIO
     xrange = range
 
 
+#TIMES = 50000000
 TIMES = 500000
 
 
-def concat_naive_func(a, n):
-    a += str(n)
-    return a
-
 def concat_naive(times):
     a = ''
+    code = compile('a += str(n)', '<string>', 'exec')
     start = time.time()
     for n in xrange(times):
-        a = concat_naive_func(a, n)
+        exec code
     delta = time.time() - start
     return delta
 
-def concat_array_func(a, n):
-    a.fromstring(str(n))
+# *REALLY* slow!
+def concat_mutable(times):
+    a = MutableString()
+    code = compile('a += str(n)', '<string>', 'exec')
+    start = time.time()
+    for n in xrange(times):
+        exec code
+    delta = time.time() - start
+    return delta
 
 def concat_array(times):
     a = array('c')
+    code = compile('a.fromstring(str(n))', '<string>', 'exec')
     start = time.time()
     for n in xrange(times):
-        concat_array_func(a, n)
+        exec code
+#        a.fromstring(str(n))
     a = ''.join(a)
     delta = time.time() - start
     return delta
-
-def concat_join_func(a, n):
-    return a.append(str(n))
 
 def concat_join(times):
     a = []
+    code = compile('a.append(str(n))', '<string>', 'exec')
     start = time.time()
     for n in xrange(times):
-        concat_join_func(a, n)
+        exec code
+#        a.append(str(n))
     a = ''.join(a)
     delta = time.time() - start
     return delta
 
-def concat_stringio_func(a, n):
-    a.write(str(n))
-
 def concat_stringio(times):
     a = StringIO()
+    code = compile('a.write(str(n))', '<string>', 'exec')
     start = time.time()
     for n in xrange(times):
-        concat_stringio_func(a, n)
+        exec code
+#        a.write(str(n))
     a = a.getvalue()
     delta = time.time() - start
     return delta
 
+def concat_comprehension(times):
+    start = time.time()
+    a = [str(n) for n in xrange(times)]
+    a = ''.join(a)
+    delta = time.time() - start
+    return delta
+
 if __name__ == '__main__':
+    time.sleep(0.5)
     print('Using Python %s on %s' % (platform.python_version(), platform.platform()))
     print('For %d concatenations:' % TIMES)
     result = concat_naive(TIMES)
     print('        naive: %5.2fs' % result)
-#    time.sleep(1)
+    time.sleep(1)
 #    result = concat_array(TIMES)
 #    print('        array: %5.2fs' % result)
-    time.sleep(1)
+#    time.sleep(1)
     result = concat_join(TIMES)
     print('         join: %5.2fs' % result)
     time.sleep(1)
     result = concat_stringio(TIMES)
     print('     stringio: %5.2fs' % result)
     time.sleep(1)
+    result = concat_comprehension(TIMES)
+    print('comprehension: %5.2fs' % result)
+    time.sleep(1)
+
+
