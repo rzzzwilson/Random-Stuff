8e9d60292212ba4104abf24c1ca84bad3a2de197
Ross Wilson <rzzzwilson@gmail.com>
2017-08-04 16:55:54 +0700

    Added execution times to file

--- PythonExercises/recursion/fibonacci_iter.py
+++ PythonExercises/recursion/fibonacci_iter.py
@@ -5,7 +5,7 @@ import time
 Use it like this:
 
 $ python fibonacci_iter.py 40
-Fibonacci(40)=102334155     took 31.8008261s
+Fibonacci(40)=102334155     took 0.0000339s
 """
 
 FibMemo = {0: 0, 1: 1}     # memo + base cases
@@ -15,7 +15,6 @@ def fibonacci_iter(n):
     global FibMemoMax
     while FibMemoMax < n:
         FibMemoMax += 1
-        print
         FibMemo[FibMemoMax] = FibMemo[FibMemoMax-1] + FibMemo[FibMemoMax-2]
 
     return FibMemo[n]
