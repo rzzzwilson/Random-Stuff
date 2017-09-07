64ef695ed1d9e1581217ad955482b9957fd26569
Ross Wilson <rzzzwilson@gmail.com>
2017-09-07 23:11:07 +0700

    Added comments about windows drive letter

--- tkinter/limit_path_length.py
+++ tkinter/limit_path_length.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 """
-An example of limiting a path length for display in a Label widget.
+An example of limiting a path length for display in a widget.
+
+This is for UNIX-like filesystems.  On Windows would need extra code to
+handle the drive.
 """
 
 import sys
@@ -31,6 +34,7 @@ def limit_path(path, limit):
         path = head
 
     # create the default path 'head' plus terminal file
+    # for Windows we would look for the drive letter
     head_path = os.path.join('/', path_list.pop(0), '...')
     tail_path = path_list.pop()
     result = os.path.join(head_path, tail_path)
