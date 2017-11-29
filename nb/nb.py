010099ede6dead3b707c9b29fdeb5c48f6e6a852
Ross Wilson <rzzzwilson@gmail.com>
2017-11-29 20:34:04 +0700

    Embedded imports into nb.py

--- nb/nb.py
+++ nb/nb.py
@@ -10,13 +10,13 @@ import os
 import sys
 import time
 import shutil
+import string
 import platform
-import logger
-try:
-    from tkinter_error import tkinter_error
-except ImportError:
-    tkinter_error = print
+import datetime
+import traceback
+from tkinter import *
 from subprocess import Popen, PIPE, STDOUT
+
 
 # Requirements:
 #   . Must find mounted NOTEBOOK automatically
@@ -48,6 +48,297 @@ elif UName == 'Darwin':
     ScriptPath = os.path.expanduser('~/bin/nb')
 else:
     abort("Don't handle Windows yet!")
+
+'''
+A small function to put an error message on the screen with Tkinter.
+
+Used by GUI programs started from a desktop icon.
+'''
+
+
+def tkinter_error(msg, title=None):
+    """Show an error message in a Tkinter dialog.
+
+    msg    text message to display (may contain newlines, etc)
+    title  the window title (defaults to 'ERROR')
+
+    The whole point of this is to get *some* output from a python GUI
+    program when run from an icon double-click.  We use Tkinter since it's
+    part of standard python and we may be trying to say something like:
+
+        +-----------------------------+
+        |  you must install wxPython  |
+        +-----------------------------+
+
+    Under Linux and OSX we can run the program from the commandline and we would
+    see printed output.  Under Windows that's hard to do, hence this code.
+
+    NOTE: For some reason, Ubuntu python doesn't have tkinter installed as
+    part of the base install.  Do "sudo apt-get install python-tk".
+    """
+
+    ######
+    # Define the Application class
+    ######
+
+    class Application(Frame):
+        def createWidgets(self):
+            self.LABEL = Label(self, text=self.text, font=("Courier", 14))
+            self.LABEL["fg"] = "black"
+            self.LABEL["bg"] = "yellow"
+            self.LABEL["justify"] = "left"
+            self.LABEL.pack()
+
+        def __init__(self, text, master=None):
+            self.text = text
+            Frame.__init__(self, master)
+            self.pack()
+            self.createWidgets()
+            self.tkraise()
+
+
+    # set the title string
+    if title is None:
+        title = 'ERROR'
+
+    # get the message text
+    msg = '\n' + msg.strip() + '\n'
+
+    msg = msg.replace('\r', '')
+    msg = msg.replace('\n', '   \n   ')
+
+    app = Application(msg)
+    app.master.title(title)
+    app.mainloop()
+
+
+"""
+A simple logger.
+
+Simple usage:
+    import logger
+    log = logger.Log('my_log.log', logger.Log.DEBUG)
+    log('A line in the log at the default level (DEBUG)')   # simple
+    log('A log line at WARN level', logger.Log.WARN)        # hard to use
+    log.info('log line issued at INFO level')               # best if using level
+
+Based on the 'borg' recipe from [http://code.activestate.com/recipes/66531/].
+
+Log levels styled on the Python 'logging' module.
+
+Log output includes the module and line # of the log() call.
+"""
+
+
+################################################################################
+# A simple (?) logger.
+################################################################################
+
+class Log(object):
+
+    __shared_state = {}                # this __dict__ shared by ALL instances
+
+    # the predefined logging levels
+    CRITICAL = 50
+    ERROR = 40
+    WARN = 30
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
+
+    MAXLEVEL = CRITICAL
+    MINLEVEL = NOTSET
+
+    # dict to convert logging level back to symbolic name
+    _level_num_to_name = {
+                          NOTSET: 'NOTSET',
+                          DEBUG: 'DEBUG',
+                          INFO: 'INFO',
+                          WARN: 'WARN',
+                          ERROR: 'ERROR',
+                          CRITICAL: 'CRITICAL',
+                         }
+
+    # dict to convert symbolic name to logging level
+    # just invert _level_num_to_name
+    _name_to_level_num = {value:key for (key, value) in _level_num_to_name.items()}
+
+    # default maximum length of filename (enforced)
+    DefaultMaxFname = 15
+
+
+    def __init__(self, logfile=None, level=NOTSET, append=False,
+                 max_fname=DefaultMaxFname):
+        """Initialise the logging object.
+
+        logfile  the path to the log file
+        level    logging level - don't log below this level
+        append   True if log file is appended to
+        """
+
+        # make sure we have same state as all other log objects
+        self.__dict__ = Log.__shared_state
+
+        # set some initial state
+        self.max_fname = max_fname
+        self.sym_level = 'NOTSET'      # set in call to check_level()
+        self.level = self.check_level(level)
+
+        # if not given logfile name, make one up
+        if logfile is None:
+            logfile = '%s.log' % __name__
+
+        # get correct options for rewrite or append of logfile
+        log_options = 'w'
+        if append:
+            log_options = 'a'
+
+        # test if we can use the file
+        try:
+            self.logfd = open(logfile, log_options)
+            self.logfd.close()
+        except IOError:
+            # assume we have readonly filesystem
+            basefile = os.path.basename(logfile)
+            if sys.platform == 'win32':
+                logfile = os.path.join('C:\\', basefile)
+            else:
+                logfile = os.path.join('~', basefile)
+
+        # try to open logfile again
+        self.logfd = open(logfile, log_options)
+
+        self.logfile = logfile
+
+        # announce time+date of opening logging and logging level
+        self.debug('='*55)
+        self.debug('Log started on %s, log level=%s'
+                   % (datetime.datetime.now().ctime(),
+                      self._level_num_to_name[level]))
+        self.debug('-'*55)
+
+        # finally, set some internal state
+        self.set_level(self.level)
+
+    def check_level(self, level):
+        """Check the level value for legality.
+
+        level  a numeric logging level
+
+        If 'level' is invalid, raise Exception.  If valid, return value.
+        """
+
+        try:
+            level = int(level)
+        except ValueError:
+            msg = "Logging level invalid: '%s'" % str(level)
+            print(msg)
+            raise Exception(msg)
+
+        if not self.NOTSET <= level <= self.CRITICAL:
+            msg = "Logging level invalid: '%s'" % str(level)
+            print(msg)
+            raise Exception(msg)
+
+        return level
+
+    def set_level(self, level):
+        """Set logging level."""
+
+        level = self.check_level(level)
+
+        # convert numeric level to symbolic
+        sym = self._level_num_to_name.get(level, None)
+        if sym is None:
+            # not recognized symbolic but it's legal, so interpret as 'XXXX+2'
+            sym_10 = 10 * (level/10)
+            sym_rem = level - sym_10
+            sym = '%s+%d' % (self._level_num_to_name[sym_10], sym_rem)
+
+        self.level = level
+        self.sym_level = sym
+
+        self.critical('Logging level set to %02d (%s)' % (level, sym))
+
+    def __call__(self, msg=None, level=None):
+        """Call on the logging object.
+
+        msg    message string to log
+        level  level to log 'msg' at (if not given, assume self.level)
+        """
+
+        # get level to log at
+        if level is None:
+            level = self.level
+
+        # are we going to log?
+        if level < self.level or self.level < 0:
+            return
+
+        if msg is None:
+            msg = ''
+
+        # get time
+        to = datetime.datetime.now()
+        hr = to.hour
+        min = to.minute
+        sec = to.second
+        msec = to.microsecond
+
+        # caller information - look back for first module != <this module name>
+        frames = traceback.extract_stack()
+        frames.reverse()
+        try:
+            (_, mod_name) = __name__.rsplit('.', 1)
+        except ValueError:
+            mod_name = __name__
+        for (fpath, lnum, mname, _) in frames:
+            fname = os.path.basename(fpath).rsplit('.', 1)
+            if len(fname) > 1:
+                fname = fname[0]
+            if fname != mod_name:
+                break
+
+        # get string for log level
+        loglevel = self._level_num_to_name[level]
+
+        fname = fname[:self.max_fname]
+        self.logfd.write('%02d:%02d:%02d.%06d|%8s|%*s:%-4d|%s\n'
+                         % (hr, min, sec, msec, loglevel, self.max_fname,
+                            fname, lnum, msg))
+        self.logfd.flush()
+
+    def critical(self, msg):
+        """Log a message at CRITICAL level."""
+
+        self(msg, self.CRITICAL)
+
+    def error(self, msg):
+        """Log a message at ERROR level."""
+
+        self(msg, self.ERROR)
+
+    def warn(self, msg):
+        """Log a message at WARN level."""
+
+        self(msg, self.WARN)
+
+    def info(self, msg):
+        """Log a message at INFO level."""
+
+        self(msg, self.INFO)
+
+    def debug(self, msg):
+        """Log a message at DEBUG level."""
+
+        self(msg, self.DEBUG)
+
+    def __del__(self):
+        """Close the logging."""
+
+        self.logfd.close()
+        self.logfd = None
+
 
 def do_cmdline(cmd, text=''):
     """Execute program in 'cmd' and pass 'text' to STDIN.
@@ -246,13 +537,11 @@ def main(fs_check):
 
 
 if __name__ == '__main__':
-    import sys
     import getopt
-    import traceback
 
     global Quiet
 
-    log = logger.Log('nb.log', logger.Log.DEBUG)
+    log = Log('nb.log', Log.DEBUG)
 
     def usage(msg=None):
         """Print a USAGE message with optional error message.
