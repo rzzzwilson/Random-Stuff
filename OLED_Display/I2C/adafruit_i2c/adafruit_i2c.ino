a3964009bc460adffddc28553a77378e03f0aa32
Ross Wilson <rzzzwilson@gmail.com>
2018-03-30 19:15:14 +0700

    Fiddling

--- OLED_Display/I2C/adafruit_i2c/adafruit_i2c.ino
+++ OLED_Display/I2C/adafruit_i2c/adafruit_i2c.ino
@@ -1,11 +1,11 @@
-#include <Wire.h>
+#include <TinyWireM.h>
 #include "Adafruit_SH1106.h"
 
 /*
- * Drive the 128x32 OLED display using SH1106 controller
- * Uses I2C.
- */
- 
+   Drive the 128x32 OLED display using SH1106 controller
+   Uses I2C.
+*/
+
 // the connecting pins
 #define OLED_MOSI     11
 #define OLED_CLK      13
@@ -25,7 +25,7 @@
 Adafruit_SH1106 display(OLED_RESET);
 
 void setup(void)
-{                
+{
   Serial.begin(115200);
   Serial.print("Adafruit library using I2C\n");
   display.begin(SH1106_SWITCHCAPVCC, 0x3C);
@@ -42,7 +42,7 @@ void loop(void)
   static bool inverted = false;
   static long invert_count = 0;
   unsigned long start = micros();
-  
+
   display.clearDisplay();
 
   if (--invert_count <= 0)
@@ -60,25 +60,25 @@ void loop(void)
   display.setCursor(0, 0);
   display.print(delta);
   display.print("us  Adafruit I2C");
-//  display.print("123456789012345678901\n\n");
-  display.setCursor(TEXT_LEFT, TEXT_TOP+TEXT_HEIGHT);
+  //  display.print("123456789012345678901\n\n");
+  display.setCursor(TEXT_LEFT, TEXT_TOP + TEXT_HEIGHT);
   display.print("Quis custodiet");
-  display.setCursor(TEXT_LEFT, TEXT_TOP+TEXT_HEIGHT*2);
+  display.setCursor(TEXT_LEFT, TEXT_TOP + TEXT_HEIGHT * 2);
   display.print("ipsos custodes?");
 #if 0
-  display.setCursor(TEXT_LEFT, TEXT_TOP+TEXT_HEIGHT);
+  display.setCursor(TEXT_LEFT, TEXT_TOP + TEXT_HEIGHT);
   display.print("Herrgott, aber");
-  display.setCursor(TEXT_LEFT, TEXT_TOP+TEXT_HEIGHT*2);
+  display.setCursor(TEXT_LEFT, TEXT_TOP + TEXT_HEIGHT * 2);
   display.print("boshaft ist er nicht");
 #endif
-  display.setCursor(TEXT_LEFT, TEXT_TOP+TEXT_HEIGHT*4);
+  display.setCursor(TEXT_LEFT, TEXT_TOP + TEXT_HEIGHT * 4);
   display.print("        Juvenal");
-  
-  display.drawFastHLine(TEXT_LEFT/2-1, TEXT_TOP-TEXT_HEIGHT/2, SH1106_LCDWIDTH-TEXT_LEFT+1, 1);
-  display.drawFastHLine(TEXT_LEFT/2-1, SH1106_LCDHEIGHT-2, SH1106_LCDWIDTH-TEXT_LEFT+1, 1);
-  display.drawFastVLine(TEXT_LEFT/2-1, TEXT_TOP-4, 48, 1);
-  display.drawFastVLine(SH1106_LCDWIDTH-TEXT_LEFT/2, TEXT_TOP-4, 48, 1);
-  
+
+  display.drawFastHLine(TEXT_LEFT / 2 - 1, TEXT_TOP - TEXT_HEIGHT / 2, SH1106_LCDWIDTH - TEXT_LEFT + 1, 1);
+  display.drawFastHLine(TEXT_LEFT / 2 - 1, SH1106_LCDHEIGHT - 2, SH1106_LCDWIDTH - TEXT_LEFT + 1, 1);
+  display.drawFastVLine(TEXT_LEFT / 2 - 1, TEXT_TOP - 4, 48, 1);
+  display.drawFastVLine(SH1106_LCDWIDTH - TEXT_LEFT / 2, TEXT_TOP - 4, 48, 1);
+
   display.display();
 
   delta = micros() - start;
--- OLED_Display/I2C/digispark_i2c/digispark_i2c.ino
+++ OLED_Display/I2C/digispark_i2c/digispark_i2c.ino
@@ -2,26 +2,40 @@
 #include "font6x8.h"
 #include "sh1106.h"
 
+// Setting to '0' forces a full screen redraw each time in loop().
+// A '1' value draws the screen once with partial updates - faster!
+// But note the corruption with value set to '1'.
 #define REDRAW_SCREEN   0
+
+#define PIN_LED         1   // pin 1 has the LED
+#define PIN_AUDIO       5   // use pin 5 for audio sensor
 
 char buffer[32];
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.write("setup() finished\n");
   delay(40);
   sh1106_init();
 #if !REDRAW_SCREEN
   sh1106_fillscreen(0x00);
-  textxy(0, 1, "{{{{{{{{{{{{{{{{{{{{{{");
-  textxy(0, 2, "The time has come,");
-  textxy(0, 3, "the Walrus said, to");
-  textxy(0, 4, "talk of many things.");
-  textxy(0, 6, "        Lewis Carroll");
-  textxy(0, 7, "{{{{{{{{{{{{{{{{{{{{{{");
+  textxy(1, 0, "                      ");
+  textxy(1, 1, "{{{{{{{{{{{{{{{{{{{{{{");
+  textxy(1, 2, "The time has come,    ");
+  textxy(1, 3, "the Walrus said, to   ");
+  textxy(1, 4, "talk of many things.  ");
+  textxy(1, 5, "                      ");
+  textxy(1, 6, "        Lewis Carroll ");
+  textxy(1, 7, "{{{{{{{{{{{{{{{{{{{{{{");
 #endif
+
+  pinMode(PIN_AUDIO, INPUT);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+  Serial.write("setup() finished\n");
 }
 
-//void textxy(char* s)
 void textxy(int pos_x, int pos_y, char* s)
 {
   sh1106_setpos(pos_x, pos_y);
@@ -39,6 +53,14 @@ void loop()
   unsigned long start = micros();
 #if REDRAW_SCREEN
   sh1106_fillscreen(0x00);
+  textxy(1, 0, "                      ");
+  textxy(1, 1, "{{{{{{{{{{{{{{{{{{{{{{");
+  textxy(1, 2, "The time has come,    ");
+  textxy(1, 3, "the Walrus said, to   ");
+  textxy(1, 4, "talk of many things.  ");
+  textxy(1, 5, "                      ");
+  textxy(1, 6, "        Lewis Carroll ");
+  textxy(1, 7, "{{{{{{{{{{{{{{{{{{{{{{");
 #endif
   sprintf(buffer, "%uus", draw_time);
   sh1106_setpos(0, 0);
@@ -46,13 +68,16 @@ void loop()
   sprintf(buffer, "%06d", redraw_count);
   sh1106_setpos(90, 0);
   sh1106_draw_string(buffer);
-#if REDRAW_SCREEN
-  textxy(0, 1, "{{{{{{{{{{{{{{{{{{{{{{");
-  textxy(0, 2, "The time has come,");
-  textxy(0, 3, "the Walrus said, to");
-  textxy(0, 4, "talk of many things.");
-  textxy(0, 6, "        Lewis Carroll");
-  textxy(0, 7, "{{{{{{{{{{{{{{{{{{{{{{");
-#endif
   draw_time = (unsigned) micros() - start;
+
+  delay(1000);
+
+  int aud_value = digitalRead(PIN_AUDIO);
+  if (aud_value > 100)
+  {
+    Serial.write("Sound!\n");
+    digitalWrite(PIN_LED, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED, LOW);
+  }
 }
