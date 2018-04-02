e966346f8d781b442ba5838bb79bba4824588b4d
Ross Wilson <rzzzwilson@gmail.com>
2018-04-02 21:51:56 +0700

    Organizing demos

--- OLED_Display/I2C/digispark_i2c/digispark_i2c.ino
+++ OLED_Display/I2C/digispark_i2c/digispark_i2c.ino
@@ -1,46 +1,22 @@
-#include <stdlib.h>
-#include "font6x8.h"
 #include "sh1106.h"
 
 // Setting to '0' forces a full screen redraw each time in loop().
 // A '1' value draws the screen once with partial updates - faster!
 // But note the corruption with value set to '1'.
-#define REDRAW_SCREEN   0
-
-#define PIN_LED         1   // pin 1 has the LED
-#define PIN_AUDIO       5   // use pin 5 for audio sensor
+#define REDRAW_SCREEN   1
 
 char buffer[32];
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.write("setup() finished\n");
   delay(40);
   sh1106_init();
-#if !REDRAW_SCREEN
   sh1106_fillscreen(0x00);
-  textxy(1, 0, "                      ");
-  textxy(1, 1, "{{{{{{{{{{{{{{{{{{{{{{");
-  textxy(1, 2, "The time has come,    ");
-  textxy(1, 3, "the Walrus said, to   ");
-  textxy(1, 4, "talk of many things.  ");
-  textxy(1, 5, "                      ");
-  textxy(1, 6, "        Lewis Carroll ");
-  textxy(1, 7, "{{{{{{{{{{{{{{{{{{{{{{");
-#endif
-
-  pinMode(PIN_AUDIO, INPUT);
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
-  Serial.write("setup() finished\n");
 }
 
-void textxy(int pos_x, int pos_y, char* s)
-{
-  sh1106_setpos(pos_x, pos_y);
-  sh1106_draw_string(s);
-}
+//const uint8_t bmp[] PROGMEM = {0xff, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xff};
+const uint8_t bmp[] PROGMEM = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+
 
 void loop()
 {
@@ -51,33 +27,22 @@ void loop()
   
   // redraw the screen
   unsigned long start = micros();
-#if REDRAW_SCREEN
-  sh1106_fillscreen(0x00);
-  textxy(1, 0, "                      ");
-  textxy(1, 1, "{{{{{{{{{{{{{{{{{{{{{{");
-  textxy(1, 2, "The time has come,    ");
-  textxy(1, 3, "the Walrus said, to   ");
-  textxy(1, 4, "talk of many things.  ");
-  textxy(1, 5, "                      ");
-  textxy(1, 6, "        Lewis Carroll ");
-  textxy(1, 7, "{{{{{{{{{{{{{{{{{{{{{{");
-#endif
   sprintf(buffer, "%uus", draw_time);
-  sh1106_setpos(0, 0);
+  sh1106_setpos(2, 0);
   sh1106_draw_string(buffer);
   sprintf(buffer, "%06d", redraw_count);
   sh1106_setpos(90, 0);
   sh1106_draw_string(buffer);
   draw_time = (unsigned) micros() - start;
 
+  sh1106_setpos(2, 6);
+  sh1106_draw_bmp(24, 24, 32, 32, bmp);
+  
+  sh1106_setpos(2, 4);
+  sh1106_draw_string("{{{{");
+//  sh1106_draw_string("\\\\\\\\");
+  
   delay(1000);
-
-  int aud_value = digitalRead(PIN_AUDIO);
-  if (aud_value > 100)
-  {
-    Serial.write("Sound!\n");
-    digitalWrite(PIN_LED, HIGH);
-    delay(1000);
-    digitalWrite(PIN_LED, LOW);
-  }
 }
+
+
--- OLED_Display/I2C/digispark_i2c/sh1106.cpp
+++ OLED_Display/I2C/digispark_i2c/sh1106.cpp
@@ -21,14 +21,15 @@ const uint8_t sh1106_init_sequence [] PROGMEM = {	// Initialization Sequence
 	0x20, 0b00,		// Set Memory Addressing Mode
 					      // 00=Horizontal Addressing Mode; 01=Vertical Addressing Mode;
 					      // 10=Page Addressing Mode (RESET); 11=Invalid
-	0xB0,			    // Set Page Start Address for Page Addressing Mode, 0-7
+	0xB1,			    // Set Page Start Address for Page Addressing Mode, 0-7
 	0xC8,			    // Set COM Output Scan Direction
 	0x00,			    // ---set low column address
 	0x10,			    // ---set high column address
 	0x40,			    // --set start line address
 	0x81, 0x3F,		// Set contrast control register
 	0xA1,			    // Set Segment Re-map. A0=address mapped; A1=address 127 mapped. 
-	0xA6,			    // Set display mode. A6=Normal; A7=Inverse
+  0xA6,         // Set display mode. A6=Normal; A7=Inverse
+//  0xA7,         // Set display mode. A6=Normal; A7=Inverse
 	0xA8, 0x3F,		// Set multiplex ratio(1 to 64)
 	0xA4,			    // Output RAM to Display
 					      // 0xA4=Output follows RAM content; 0xA5,Output ignores RAM content
@@ -126,7 +127,7 @@ void sh1106_setpos(uint8_t x, uint8_t y)
 {
 	sh1106_send_command_start();
 	sh1106_send_byte(0xb0 + y);
-	sh1106_send_byte(((x & 0xf0) >> 4) | 0x10); // | 0x10
+  sh1106_send_byte(((x & 0xf0) >> 4) | 0x10); // | 0x10
   sh1106_send_byte((x & 0x0f));               // | 0x01
 	sh1106_send_command_stop();
 }
@@ -156,7 +157,7 @@ void sh1106_fillscreen(uint8_t fill)
 		sh1106_send_command(0x00);		  // low column start address
 		sh1106_send_command(0x10);		  // high column start address
 		sh1106_send_data_start();
-		for (uint8_t n = 0; n < 128; n++)
+		for (uint8_t n = 0; n < 128+2; n++)
 		{
 			sh1106_send_byte(fill);
 		}
@@ -203,11 +204,13 @@ void sh1106_draw_bmp(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, const uint8
 {
 	uint16_t j = 0;
 	uint8_t y;
-  
+
+#if JUNK
 	if (y1 % 8 == 0)
 	  y = y1 / 8;
 	else
 	  y = y1 / 8 + 1;
+#endif
    
 	for (y = y0; y < y1; y++)
 	{
