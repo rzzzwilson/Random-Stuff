d678eb6bcae4d033ba8093863f756bf4a9c7cb5c
Ross Wilson <rzzzwilson@gmail.com>
2018-08-01 16:19:27 +0700

    Commit all changed executables, remove later

--- sdl/07_texture_loading_and_rendering/07_texture_loading_and_rendering.c
+++ sdl/07_texture_loading_and_rendering/07_texture_loading_and_rendering.c
@@ -4,8 +4,8 @@
 #include <stdbool.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 512;
+const int SCREEN_HEIGHT = 512;
 
 //Starts up SDL and creates window
 bool init();
---
+++ sdl/README.txt
@@ -0,0 +1,10 @@
+Tutorial
+--------
+
+http://lazyfoo.net/tutorials/SDL/index.php#Geometry%20Rendering
+
+To build
+--------
+
+Use the Makefile in each directory.
+
