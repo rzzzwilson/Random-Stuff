0a4b2822f7582602026dbcbfe3e544995d69efb6
rzzzwilson <rzzzwilson@gmail.com>
2019-06-09 13:38:07 +0700

    Added tar.gz data

--- python_zipfile/README.rst
+++ python_zipfile/README.rst
@@ -1,14 +1,25 @@
 The code here investigates using a zipped directory to access
 cached objects like tiles.  The idea is to see if it's feasible
-to store the pySlip GMT tiles in the original zipped form and
-not need to unzip it before using it.  If it works, get some
-idea of the relative speeds zipped/unzipped.
+to store the pySlip GMT tiles in the original zipped (or *.tar.gz*)
+form and not need to unzip it before using it.  If it works, get some
+idea of the relative speeds zipped/tarred/unzipped.
 
 Results
 -------
 
 The final timing show that reading tiles from a zipped directory is
-between 2 and 3 times slower than reading from the unzipped directory.
+about teice as slow as reading from the unzipped directory.  The tarred
+directory was about 300 times slower than reading the unzipped directory.
+
+```
+$ python3 test.py
+
+unzipped took  0.26s
+  zipped took  0.58s
+  tarred took 76.52s
+
+Data the same no matter what the source
+```
 
 So we will keep the unzipped *~/gmt_local_tiles* directory for pySlip,
 but keep the unzipped approach in mind.
--- python_zipfile/test.py
+++ python_zipfile/test.py
@@ -3,6 +3,7 @@ Compare the speed of accessing pySlip tiles from:
 
 * an unzipped tiles directory
 * a zipped tiles file
+# same data but *.tar.gz
 
 Also chck that the data read is the same no matter what method
 it is read by.
@@ -11,19 +12,21 @@ it is read by.
 import os
 import time
 import zipfile
+import tarfile
 
 
 TilesUnzipped = os.path.abspath(os.path.expanduser('~/gmt_local_tiles'))
 TilesZipped = os.path.abspath('./gmt_local_tiles.zip')
+TilesTarred = os.path.abspath('./gmt_local_tiles.tar.gz')
 
 ZoomLevel = 4
 MaxTilesWide = 32
 MaxTileHigh = 16
-Loop = 100
+Loop = 10
 
 UnzippedData = []
 ZippedData = []
-
+TarredData = []
 def get_unzipped():
     """Function that gets a number of tiles from an unzipped dicretory."""
 
@@ -38,24 +41,48 @@ def get_unzipped():
 def get_zipped():
     """Function that gets a number of tiles from a zipped directory."""
 
-    for _ in range(Loop):
-        with zipfile.ZipFile(TilesZipped) as zf:
+    with zipfile.ZipFile(TilesZipped) as zf:
+        for _ in range(Loop):
             for x in range(MaxTilesWide):
                 for y in range(MaxTileHigh):
                     path = os.path.join(f'gmt_local_tiles/{ZoomLevel}/{x}/{y}.png')
                     data = zf.read(path)
                     ZippedData.append(data)
 
+def get_tarred():
+    """Function that gets a number of tiles from a tar.gz directory."""
+
+    with tarfile.open(TilesTarred, 'r') as zf:
+        for _ in range(Loop):
+            for x in range(MaxTilesWide):
+                for y in range(MaxTileHigh):
+                    path = os.path.join(f'gmt_local_tiles/{ZoomLevel}/{x}/{y}.png')
+                    data = zf.extractfile(path).read()
+                    TarredData.append(data)
+
+print()
 start = time.time()
 get_unzipped()
 delta = time.time() - start
-print(f'unzipped took {delta:.2f}s')
+print(f'unzipped took {delta:5.2f}s')
 
 start = time.time()
 get_zipped()
 delta = time.time() - start
-print(f'  zipped took {delta:.2f}s')
+print(f'  zipped took {delta:5.2f}s')
+
+start = time.time()
+get_tarred()
+delta = time.time() - start
+print(f'  tarred took {delta:5.2f}s')
 
 # compare the two lists of tile data to make sure they are the same
+error = False
 if UnzippedData != ZippedData:
     print(f'\nData read from unzipped source is NOT THE SAME as data from zipped source')
+    error = True
+if UnzippedData != TarredData:
+    print(f'\nData read from unzipped source is NOT THE SAME as data from tar.gz source')
+    error = True
+if not error:
+    print(f'\nData the same no matter what the source')
