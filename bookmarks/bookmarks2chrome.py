c7f21b1257dca1629c2a1089459aed0ff04d9247
rzzzwilson <rzzzwilson@gmail.com>
2019-07-23 22:14:50 +0700

    Start generating bookmark HTML

--- bookmarks/bookmarks2chrome.py
+++ bookmarks/bookmarks2chrome.py
@@ -8,8 +8,13 @@ Usage: bookmarks2chrome.py <data file> <HTML file>
 where <data file>    is the bookmarks data file to convert
 where <HTML file>    is the path to the output Chrome HTML bookmarks file
 
-The Chrome HTML bookmarks file format is that produced by the Chrome
-"export bookmarks" function.
+The Chrome HTML bookmarks file format is one or more lines:
+
+    path/to/bookmark@URL
+
+where path/to/bookmark is a '/' delimited bookmark folder path with the last
+                           element of the path being the bookmark title,
+and   URL              is the associated bookmark URL
 """
 
 HTML_Header = """<!DOCTYPE NETSCAPE-Bookmark-file-1>
@@ -22,16 +27,15 @@ HTML_Header = """<!DOCTYPE NETSCAPE-Bookmark-file-1>
 <DL><p>
 """
 
-HTML_Footer = """</DL><p>"""
+HTML_Footer = """</DL><p>\n"""
 
-New_Folder = """<DT><H3>FOLDERNAME_PLACE</H3>
-<DL><p>
-"""
+New_Folder = """%s<DT><H3>%s</H3><DL><p>\n"""
 
-End_Folder = """</DL><p>"""
+End_Folder = """%s</DL><p>\n"""
 
-New_Bookmark = """<DT><A HREF="HREF_PLACE">NAME_PLACE</A>
-"""
+New_Bookmark = """<DT><A HREF="%s">%s</A>\n"""
+
+indent_size = 4
 
 
 def process_bookmarks(in_handle, out_handle):
@@ -48,63 +52,77 @@ def process_bookmarks(in_handle, out_handle):
     # print the HTML header
     out_handle.write(HTML_Header)
 
-    # set indent for first bookmark
-    indent_count = 4
-    indent = ' ' * indent_count
+    # how deep we are in folders
+    depth = 0
 
     # process each line in the input file
-    prev_path = None
+    prev_path = ''
     for (lnum, line) in enumerate(in_handle):
+        line = line.strip()
+        if line.startswith('#'):
+            print(f'Ignoring: {line}')
+            continue
+
+        print('###########################################')
         print(f'line: {line}')
         try:
-            (path, url) = line.strip().split('\t')
+            (path, url) = line.strip().split('@')
         except ValueError:
             # bad split, report line
             print(f'line {lnum+1} has bad format: {line}')
             return 1
 
-        # split path into path + bookmark
-        (path, bookmark) = os.path.split(path)
+        # split path into path + title
+        (path, title) = os.path.split(path)
         if not path:
             # MUST have something in path
             print(f'line {lnum+1} has bad format: {line}')
             return 1
 
-        print(f'path={path}, bookmark={bookmark}')
+        print(f'prev_path={prev_path}, path={path}, title={title}, url={url}')
 
-        if path != prev_path:
-            # change in folder, get divided paths
-            div_path = path.split('/')
-            if prev_path is None:
-                div_prev_path = []
-            else:
-                div_prev_path = prev_path.split('/')
-            print(f'div_path={div_path}, div_prev_path={div_prev_path}')
-            print(f'prev_path={prev_path}, div_prev_path={div_prev_path}')
+        if not prev_path:
+            # first folder, create new folder hierachy
+            div_path = path.split('/')      # get split path
+            for folder in div_path:
+                out_handle.write(New_Folder % (" " * depth, folder))
+                depth += indent_size
 
-            while len(div_path) < len(div_prev_path):
-                print('Deleting older deep paths')
-                # close one or more folders
-                indent_count -= 4
-                indent = ' ' * indent_count
-                out_handle.write(f'{indent}{End_Folder}\n')
-                del div_prev_path[-1]
-
-            while div_prev_path and (div_path != div_prev_path):
-                print('Close one or more folders')
-                # close one or more folders
-                indent_count -= 4
-                indent = ' ' * indent_count
-                out_handle.write(f'{indent}{End_Folder}\n')
-                del div_prev_path[-1]
-                del div_path[-1]
-
+            # create first bookmark in new folder stack
+            out_handle.write(f'{" " * depth}<DT><A HREF="{title}">{url}</A>\n')
         else:
-            # next bookmark in this folder
-            out_handle.write(f'{indent}<DT><A HREF="{url}">{bookmark}</A>\n')
+            # have previous path, anything in common?
+            prefix = os.path.commonpath([path, prev_path])
+            print(f'prefix={prefix}, prev_path={prev_path}')
+            if prefix == prev_path:
+                # same folder, so just add new bookmark
+                out_handle.write(f'{" " * depth}<DT><A HREF="{title}">{url}</A>\n')
+            else:
+                # new folder, unwind back to common prefix (which may be empty)
+                div_prefix = prefix.split('/')
+                div_prev = prev_path.split('/')
+                while div_prev != div_prefix:
+                    print(f'loop: div_prev={div_prev}, div_prefix={div_prefix}')
+                    # close one folder
+                    depth -= indent_size
+                    out_handle.write(End_Folder % (" " * depth))
+                    if div_prev:
+                        del div_prev[-1]
+                # add new folder(s)
+                for folder in div_path[len(prefix):]:
+                    out_handle.write(New_Folder % (" " * depth, folder))
+                    depth += indent_size
+                # add new bookmark
+                out_handle.write(f'{" " * depth}<DT><A HREF="{title}">{url}</A>\n')
 
         # remember the last path
         prev_path = path
+
+    # close outstanding folders
+    while depth > 0:
+        print(f'loop, depth={depth}')
+        depth -= indent_size
+        out_handle.write(End_Folder % (" " * depth))
 
     # print the HTML footer
     out_handle.write(HTML_Footer)
